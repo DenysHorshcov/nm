ba867b131899fa24c47cad8119bc3b04610d1b3c
Denys <80120774+DenysHorshcov@users.noreply.github.com>
2025-11-27 19:36:25 +0200

    Refactor iterative and bisection methods
    Refactor iterative and bisection methods to use fixed iterations and improve output formatting.

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,36 +1,35 @@
-import math
+import math 
 
 def f(x):
     return x**3 - 3*x**2 - 14*x - 8
 
-# φ(x) для методу простої ітерації
 def phi(x):
     return (3*x*x + 14*x + 8)**(1/3)
+
+
 
 def iterative_method():
     a = 5
     b = 6
     x0 = (a + b) / 2
     e = 10**-8
+    n_apr = 13   # теоретично достатньо 13 ітерацій
 
-    print(f"\n{'n':<5} | {'x':<20} | {'f(x)':<20} | {'|xn-xp|':<20}")
+    print(f"\nМетод простої ітерації (13 кроків)")
+    print(f"{'n':<5} | {'x':<20} | {'f(x)':<20} | {'|xn-xp|':<20}")
     print("-" * 80)
 
     x = x0
     print(f"{0:<5} | {x:<20} | {f(x):<20} | {'-':<20}")
 
-    for n in range(1, 1000):  # максимум 1000 ітерацій на випадок розбігу
+    for n in range(1, n_apr + 1):
         xp = x
         x = phi(xp)
         diff = abs(x - xp)
 
         print(f"{n:<5} | {x:<20} | {f(x):<20} | {diff:<20}")
 
-        if diff < e:
-            break
-
-    print(f"\nFinal approximation: x ≈ {x}")
-    print(f"Iterations: {n}\n")
+    print(f"\nFinal approximation after {n_apr} steps: x ≈ {x}\n")
 
 
 def bisection_method():
@@ -38,9 +37,10 @@ def bisection_method():
     b = 6
     e = 10**-8
 
-    n_apr = math.ceil(math.log((b - a) / e) / math.log(2))
+    n_apr = 14  # використовуємо саме априорну оцінку
 
-    print(f"\n{'n':<5} | {'a':<15} | {'b':<15} | {'x':<20} | {'f(x)':<20}")
+    print(f"\nМетод бісекції (14 кроків)")
+    print(f"{'n':<5} | {'a':<15} | {'b':<15} | {'x':<20} | {'f(x)':<20}")
     print("-" * 90)
 
     for i in range(1, n_apr + 1):
@@ -49,25 +49,21 @@ def bisection_method():
 
         print(f"{i:<5} | {a:<15} | {b:<15} | {x:<20} | {fx:<20}")
 
-        # вибір нового проміжку
         if fx * f(a) < 0:
             b = x
         else:
             a = x
 
-        if (b - a) < 2 * e:
-            break
+    print(f"\nFinal approximation after {n_apr} steps: x ≈ {x}\n")
 
-    print(f"\nFinal approximation: x ≈ {x}")
-    print(f"Iterations: {i}\n")
 
 
 while True:
     mode = input(
-        "\nSelect algorithm to solve x^3 - 3x^2 - 14x - 8 = 0:\n"
+        "\nSelect algorithm:\n"
         " 1: Iterative method\n"
         " 2: Bisection method\n"
-        " 3: Exit program\n"
+        " 3: Exit\n"
         " > "
     )
 
